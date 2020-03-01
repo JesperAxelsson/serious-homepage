8e679b6a13407f690c58f0a4378c608dc8900942
Jesper Axelsson <jesperaxe@gmail.com>
2020-03-01 17:44:04 +0100

    Make requests

--- front-end/src/views/recipies.rs
+++ front-end/src/views/recipies.rs
@@ -1,15 +1,97 @@
+#![allow(dead_code)]
+use std::fmt::{Error, Formatter};
+use std::future::Future;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response, Window};
+
 use yew::prelude::*;
+
+/// This method processes a Future that returns a message and sends it back to the component's
+/// loop.
+///
+/// # Panics
+/// If the future panics, then the promise will not resolve, and will leak.
+// #[cfg(all(target_arch = "wasm32", not(target_os = "wasi"), not(cargo_web)))]
+pub fn send_future<COMP: Component, F>(link: &ComponentLink<COMP>, future: F)
+where
+    F: Future<Output = COMP::Message> + 'static,
+{
+    use wasm_bindgen_futures::future_to_promise;
+
+    let link = link.clone();
+    let js_future = async move {
+        link.send_message(future.await);
+        Ok(JsValue::NULL)
+    };
+
+    future_to_promise(js_future);
+}
+
+/// Something wrong has occurred while fetching an external resource.
+#[derive(Debug, Clone, PartialEq)]
+pub struct FetchError {
+    err: JsValue,
+}
+impl std::fmt::Display for FetchError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        std::fmt::Debug::fmt(&self.err, f)
+    }
+}
+impl std::error::Error for FetchError {}
+
+impl From<JsValue> for FetchError {
+    fn from(value: JsValue) -> Self {
+        FetchError { err: value }
+    }
+}
+
+/// The possible states a fetch request can be in.
+pub enum FetchState<T> {
+    NotFetching,
+    Fetching,
+    Success(T),
+    Failed(FetchError),
+}
+
+/// Fetches markdown from Yew's README.md.
+///
+/// Consult the following for an example of the fetch api by the team behind web_sys:
+/// https://rustwasm.github.io/wasm-bindgen/examples/fetch.html
+async fn fetch_markdown() -> Result<String, FetchError> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let request = Request::new_with_str_and_init(
+        // "https://raw.githubusercontent.com/yewstack/yew/master/README.md",
+        "http://localhost:3030/todos",
+        &opts,
+    )?;
+
+    let window: Window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    assert!(resp_value.is_instance_of::<Response>());
+
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    let text = JsFuture::from(resp.text()?).await?;
+    Ok(text.as_string().unwrap())
+}
 
 pub struct Recipies {
     link: ComponentLink<Self>,
+    markdown: FetchState<String>,
 }
 
 pub enum Msg {
+    SetMarkdownFetchState(FetchState<String>),
+    GetMarkdown,
 }
 
 #[derive(Clone, PartialEq, Properties)]
-pub struct Props {
-}
+pub struct Props {}
 
 impl Component for Recipies {
     type Message = Msg;
@@ -18,16 +100,34 @@ impl Component for Recipies {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Recipies {
             link,
+            markdown: FetchState::NotFetching,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        // match msg {
-        //     Msg::Clicked => {
-        //         self.onsignal.emit(());
-        //     }
-        // }
-        false
+        match msg {
+            Msg::GetMarkdown => {
+                println!("New fetch!");
+                let future = async {
+                    match fetch_markdown().await {
+                        Ok(md) => Msg::SetMarkdownFetchState(FetchState::Success(md)),
+                        Err(err) => Msg::SetMarkdownFetchState(FetchState::Failed(err)),
+                    }
+                };
+
+                send_future(&self.link, future);
+
+                self.link
+                    .send_message(Msg::SetMarkdownFetchState(FetchState::Fetching));
+
+                false
+            }
+            Msg::SetMarkdownFetchState(fetch_state) => {
+                println!("SEt_fetch!");
+                self.markdown = fetch_state;
+                true
+            }
+        }
     }
 
     // fn change(&mut self, props: Self::Properties) -> ShouldRender {
@@ -37,9 +137,22 @@ impl Component for Recipies {
     // }
 
     fn view(&self) -> Html {
+        println!("View!");
         html! {
             <div>
                 { "Recipies!" }
+                {
+                match &self.markdown {
+                    FetchState::NotFetching => html! {
+                        <button onclick=self.link.callback(|_| Msg::GetMarkdown)>
+                            {"Get Markdown"}
+                        </button>
+                    },
+                    FetchState::Fetching => html! {"Fetching"},
+                    FetchState::Success(data) => html! {&data},
+                    FetchState::Failed(err) => html! {&err},
+                }
+            }
             </div>
         }
     }
