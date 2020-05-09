705c4f882645928f08cb5e481d78167fcbce2349
Jesper Axelsson <jesperaxe@gmail.com>
2020-05-09 12:03:54 +0200

    Change author

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const distPath = path.resolve(__dirname, "dist");
 module.exports = (env, argv) => {
