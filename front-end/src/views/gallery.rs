1f2e4d3f2227cf6664a30f14fdfef1a6eece2b1e
Jesper Axelsson <jesperaxe@gmail.com>
2020-09-27 17:22:27 +0200

    Fill bottom with empty divs to balance

--- front-end/src/views/gallery.rs
+++ front-end/src/views/gallery.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use yew::prelude::*;
+use crate::fetch::*;
 
 pub struct Gallery {
     link: ComponentLink<Self>,
@@ -8,8 +9,16 @@ pub struct Gallery {
 }
 
 pub enum GalleryState {
-    Main,
-    Browsing,
+    Albums,
+    Images,   
+}
+
+// #[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Album {
+    pub id: i64,
+    pub title: String,
+    pub description: String,
+    pub image_url: String,
 }
 
 pub enum Msg {}
@@ -24,7 +33,7 @@ impl Component for Gallery {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Gallery {
             link,
-            state: GalleryState::Main,
+            state: GalleryState::Albums,
         }
     }
 
@@ -44,15 +53,99 @@ impl Component for Gallery {
     }
 
     fn view(&self) -> Html {
+        let album = Album {
+            id: 1, 
+            title: "Jutenhaim 2021".to_string(),
+            description: "Things that happened".to_string(),
+            image_url: "https://images.unsplash.com/photo-1573134806390-786f85c2e828?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&h=300&q=80".to_string(),
+        };
+
+        let album2 = Album {
+            id: 1, 
+            title: "Spain".to_string(),
+            description: "Things that happened".to_string(),
+            image_url: "https://images.unsplash.com/photo-1573134806390-786f85c2e828?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&h=300&q=80".to_string(),
+        };
+
         match self.state {
-            GalleryState::Main => html! {
-                <div>
-                    { "Gallery!" }
+            GalleryState::Albums => html! {
+                // <div class="flex flex-col overflow-hidden">
+                    // <div class="h-full">
+                <div class="flex flex-col overflow-hidden h-full p-3">
+                    <div class="flex-1 overflow-y-auto">
+                        <div class="flex flex-wrap overflow-y-visible  justify-between">
+                        // <div class="overflow-y-auto overflow-x-hidden flex flex-wrap ">
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_album_card(&album) }}
+                            {{ render_album_card(&album2) }}
+                            {{ render_empty_card() }}
+                            {{ render_empty_card() }}
+                            {{ render_empty_card() }}
+                            {{ render_empty_card() }}
+                            {{ render_empty_card() }}
+                            {{ render_empty_card() }}
+                            {{ render_empty_card() }}
+                            {{ render_empty_card() }}
+                        </div>
+                    </div>
                 </div>
             },
-            GalleryState::Browsing => html! {
+            GalleryState::Images => html! {
                 <div>{ "Neat" } </div>
             },
         }
+    }
+}
+
+fn render_album_card(album: &Album) -> Html {
+    html!{
+        <div class="shadow-xl bg-darkgreen h-40 w-32 mr-2 mb-2 pb-1 flex-none  ">
+            <img src=&album.image_url class="p-1 h-32"/>
+
+            <div class="text-sm text-beige-lighter font-medium text-center">
+                {{ &album.title }}
+            </div>
+        </div>
+    }
+}
+
+fn render_empty_card() -> Html {
+    html!{
+        <div class="h-1 w-32 mr-2 mb-2 pb-1 invisible">
+            {"\u{00a0}" }
+        </div>
     }
 }
