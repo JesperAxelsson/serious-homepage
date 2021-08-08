d742761ee8524671bd4cea566aa615c6f64c8b6a
Jesper Axelsson <jesperaxe@gmail.com>
2021-08-08 14:54:42 +0200

    Add admin project

---
+++ admin/src/main.ts
@@ -0,0 +1,8 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+createApp(App).use(store).use(router).mount('#app')
---
+++ admin/src/views/gallery/Gallery.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Gallery">
+    <p>
+      Gallery and stuff
+    </p>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Gallery',
+  // components: {
+  //   HelloWorld,
+  // },
+});
+</script>
